80e75cb91afb4dc20b2a34333e930e7166256585
Unknown <ricky.vandenberg@hotmail.nl>
2018-04-24 12:49:17 +0200

    s

--- routes/routes_apiv4.js
+++ routes/routes_apiv4.js
@@ -62,7 +62,10 @@ router.post('/actors', (req, res, next) => {
     let actor = req.body;
     console.log(req.body)
 
-    const query =  "INSERT INTO actor(first_name, last_name)" + ` VALUES (${actor.first_name}, ${actor.last_name})`
+    const query = {
+        sql: 'INSERT INTO `actor`(first_name, last_name) VALUES (?, ?)',
+        values: [actor.first_name, actor.last_name ],
+    };
 
     console.log('QUERY: ' + query.sql);
     pool.connect((err, client, done) => {
